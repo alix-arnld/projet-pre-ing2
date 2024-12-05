ce5748a59bccabe5a36b4873a1125f47d7585d4e
noemiejrn <joronnoemie4@gmail.com>
2024-12-05 09:03:45 +0100

    changement1

--- main.c
+++ main.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+typedef struct station{
+    int identifiant;
+    int capacite;
+    int somme;
+    struct station* fg;
+    struct station* fd;
+    int equilibre;
+}Station;
+
+//fonction d'quilibrage
 
 int main() {
     printf("Hello, World!\n");
