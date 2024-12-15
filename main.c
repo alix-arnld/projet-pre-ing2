65fd56455c0719c8acc3b03f0e7a989d7ae3c492
lulu597 <natation0912@gmail.com>
2024-12-15 17:24:24 +0100

    chgt

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <time.h>
 
 typedef struct station{
     int identifiant;
@@ -19,11 +18,7 @@ typedef struct Arbre{
     int equilibre;
 }Arbre;
 
-//fonction d'equilibrage, creation arbre, max, min,
-// rotation(gauche,droite,double);insertion,exoste fils droit, recherche
-//fonction somme pour calculer tout les noeud de l'arbre'
-// fonction pour recuperer info sur fichoer
-
+//fonction qui va créer un nœud de l'arbre et l'initialera
 Arbre *creationArbre(int identifiant , long long capacite){
     Arbre *d=malloc(sizeof(Arbre));
     if(d==NULL){
@@ -38,13 +33,17 @@ Arbre *creationArbre(int identifiant , long long capacite){
     return d;
 }
 
+//fonction qui retournera le maximum entre les 2 valeurs en paramètre
 int max(int a, int b){
     return (a>b)?a:b;
 }
+
+//fonction qui retournera le minimum entre les 2 valeurs en paramètre
 int min(int a, int b){
     return (a<b)?a:b;
 }
 
+//fonction qui va rééquilibrer l'arbre par unr rotation simple gauche si l'élément ajouté qui déséquilibre est tout à droite
 Arbre * rotationgauche(Arbre *a){
     Arbre *pivot=a->fd;
     int eq_a=a->equilibre,eq_p=pivot->equilibre;
@@ -56,6 +55,7 @@ Arbre * rotationgauche(Arbre *a){
     return a;
 }
 
+//fonction qui va rééquilibrer l'arbre par unr rotation simple droite si l'élément ajouté qui déséquilibre est tout à gauche
 Arbre * rotationdroite(Arbre *a){
     Arbre *pivot=a->fg;
     int eq_a=a->equilibre,eq_p=pivot->equilibre;
@@ -67,16 +67,19 @@ Arbre * rotationdroite(Arbre *a){
     return a;
 }
 
+//fonction qui va rééquilibrer l'arbre si une rotation simple ne suffit pas car la hauteur est trop importante, elle va faire une rotation simple droite puis une simple gauche
 Arbre* doublerotationgauche(Arbre *a){
     a->fd= rotationdroite(a->fd);
     return rotationgauche(a);
 }
 
+//fonction qui va rééquilibrer l'arbre si une rotation simple ne suffit pas car la hauteur est trop importante, elle va faire une rotation simple gauche puis une simple droite
 Arbre*doublerotationdroite(Arbre*a){
     a->fg= rotationgauche(a->fg);
     return rotationdroite(a);
 }
 
+// fonction qui va rééquilibrer l'arbre si il ne l'est plus, en utilisant les différentes rotations en fonction des cas
 Arbre* equilibrage(Arbre *a){
     if(a->equilibre>=2) {
         if (a->fd >= 0) {
@@ -96,8 +99,7 @@ Arbre* equilibrage(Arbre *a){
     return a;
 }
 
-//inserer les capacitées et les identifiants dans l'arbre
-
+//fonction qui permet d'ajouter un noeud dans l'arbre AVL et qui rééquilibrera l'arbre après chaque ajout
 Arbre *insertionArbre(Arbre *a, int id, long long capacite, int* h){
     if (a == NULL) {
         *h = 1;
@@ -127,6 +129,7 @@ Arbre *insertionArbre(Arbre *a, int id, long long capacite, int* h){
 //procédure de recherche à partir des identifiants de la station pour ajouter la consommation du consommateur à celle totale
 void ajoutconsommation(Arbre *a, long long k, int id){
     if(a==NULL){
+        printf("l'arbre est vide");
         return;
     }
     if(id<a->station->identifiant){
@@ -142,10 +145,15 @@ void ajoutconsommation(Arbre *a, long long k, int id){
 
 //procédure qui lit les données filtrées depuis un fichier CSV en fonction d'un type donné et met à jour la consommation totale en y ajoutant les consommations associées aux consommateurs
 void recuperationconsommation(Arbre*a, char *type){
+    if(a==NULL){
+        printf("L'arbre est vide");
+        return;
+    }
     FILE *fichier=fopen("donnees_filtrees.csv","r");
     if(fichier ==NULL){
         exit(2);
     }
+
     int id_temporaire = 0;
     long long k = 0;
     if(strcmp(type,"hvb")==0){
@@ -165,7 +173,7 @@ void recuperationconsommation(Arbre*a, char *type){
     }
 }
 
-//
+// Fonction qui permet de récupérer les stations dans le fichier pour ensuite les ajouter dans un arbre AVL
 Arbre* recuperationfichier(char * nomfichier, char *type, char *consommateur){
     FILE *fichier=fopen(nomfichier,"r");
     if(fichier ==NULL){
@@ -186,7 +194,8 @@ Arbre* recuperationfichier(char * nomfichier, char *type, char *consommateur){
 void sommeconsommation(Arbre* a){
 
     if(a==NULL){
-        exit(2);
+        printf("L'arbre est vide");
+        return;
     }
     sommeconsommation(a->fg);
     a->station->analyse = (a->station->capacite - a->station->consommation);
@@ -194,81 +203,106 @@ void sommeconsommation(Arbre* a){
 }
 
 //procédure qui va créer les fichiers avec les données de surproduction ou non de chaque station en fonction de ce que l'utilisateur aura demandé
-void creationfichieranalyse(Arbre *a, char *type, char * pareil){
+void creationfichieranalyse(Arbre *a, char *type, char * consommateur){
+    if(a==NULL){
+        printf("L'arbre est vide");
+        return;
+    }
     if(strcmp(type,"hvb")==0){
-        FILE *hvb = fopen("hvb_comp.csv", "w");
-        if (hvb == NULL) {
-            exit(1);
+        FILE *fichier = fopen("hvb_comp.csv", "w");
+        if (fichier == NULL) {
+            exit(2);
         }
         printf("station:capacité:consommation totale des consommateurs:analyse de la comsommation");
-        fprintf(hvb,"%d:%lld:%lld:%lld",a->station->identifiant,a->station->capacite,a->station->consommation,a->station->analyse);
+        fprintf(fichier,"%d:%lld:%lld:%lld",a->station->identifiant,a->station->capacite,a->station->consommation,a->station->analyse);
         if(a->fg!=NULL){
-            creationfichieranalyse(a->fg, type,pareil);
+            creationfichieranalyse(a->fg, type,consommateur);
         }
-        if(a->fd!=NULL){
-            creationfichieranalyse(a->fd, type,pareil);
+        else if(a->fd!=NULL){
+            creationfichieranalyse(a->fd, type,consommateur);
+        }
+        else{
+            fclose(fichier);
+            return;
         }
     }
     if(strcmp(type,"hva")==0){
-        FILE *hva = fopen("hva_comp.csv", "w");
-        if (hva == NULL) {
-            exit(1);
+        FILE *fichier = fopen("hva_comp.csv", "w");
+        if (fichier == NULL) {
+            exit(2);
         }
-        printf("tation:capacité:consommation totale des consommateurs:analyse de la comsommation");
-        fprintf(hva, "%d:%lld:%lld:%lld",a->station->identifiant,a->station->capacite,a->station->consommation,a->station->analyse);
+        printf("station:capacité:consommation totale des consommateurs:analyse de la comsommation");
+        fprintf(fichier, "%d:%lld:%lld:%lld",a->station->identifiant,a->station->capacite,a->station->consommation,a->station->analyse);
         if(a->fg!=NULL){
-            creationfichieranalyse(a->fg, type,pareil);
+            creationfichieranalyse(a->fg, type,consommateur);
         }
-        if(a->fd!=NULL){
-            creationfichieranalyse(a->fd, type,pareil);
+        else if(a->fd!=NULL){
+            creationfichieranalyse(a->fd, type,consommateur);
+        }
+        else{
+            fclose(fichier);
+            return;
         }
     }
     if(strcmp(type,"lv")==0){
-        if(strcmp(pareil,"comp")==0){
-            FILE *lv = fopen("lv_comp.csv", "w");
-            if (lv == NULL) {
-                exit(1);
+        if(strcmp(consommateur,"comp")==0){
+            FILE *fichier = fopen("lv_comp.csv", "w");
+            if (fichier == NULL) {
+                exit(2);
             }
-            printf("tation:capacité:consommation totale des consommateurs:analyse de la comsommation");
-            fprintf(lv,"%d:%lld:%lld:%lld",a->station->identifiant,a->station->capacite,a->station->consommation,a->station->analyse);
+            printf("station:capacité:consommation totale des consommateurs:analyse de la comsommation");
+            fprintf(fichier,"%d:%lld:%lld:%lld",a->station->identifiant,a->station->capacite,a->station->consommation,a->station->analyse);
             if(a->fg!=NULL){
-                creationfichieranalyse(a->fg, type,pareil);
+                creationfichieranalyse(a->fg, type,consommateur);
             }
-            if(a->fd!=NULL){
-                creationfichieranalyse(a->fd, type,pareil);
+            else if(a->fd!=NULL){
+                creationfichieranalyse(a->fd, type,consommateur);
+            }
+            else{
+                fclose(fichier);
+                return;
             }
         }
-        if(strcmp(pareil,"indiv")==0) {
-            FILE *lv = fopen("lv_indiv.csv", "w");
-            if (lv == NULL) {
-                exit(1);
+        if(strcmp(consommateur,"indiv")==0) {
+            FILE *fichier = fopen("lv_indiv.csv", "w");
+            if (fichier == NULL) {
+                exit(2);
             }
-            printf("tation:capacité:consommation totale des consommateurs:analyse de la comsommation");
-            fprintf(lv,"%d:%lld:%lld:%lld",a->station->identifiant,a->station->capacite,a->station->consommation,a->station->analyse);
+            printf("station:capacité:consommation totale des consommateurs:analyse de la comsommation");
+            fprintf(fichier,"%d:%lld:%lld:%lld",a->station->identifiant,a->station->capacite,a->station->consommation,a->station->analyse);
             if(a->fg!=NULL){
-                creationfichieranalyse(a->fg, type,pareil);
+                creationfichieranalyse(a->fg, type,consommateur);
             }
-            if(a->fd!=NULL){
-                creationfichieranalyse(a->fd, type,pareil);
+            else if(a->fd!=NULL){
+                creationfichieranalyse(a->fd, type,consommateur);
+            }
+            else{
+                fclose(fichier);
+                return;
             }
         }
-        if(strcmp(pareil,"all")==0) {
-            FILE *lv = fopen("lv_all.csv", "w");
-            if (lv == NULL) {
-                exit(1);
+        if(strcmp(consommateur,"all")==0) {
+            FILE *fichier = fopen("lv_all.csv", "w");
+            if (fichier == NULL) {
+                exit(2);
             }
-            printf("tation:capacité:consommation totale des consommateurs:analyse de la comsommation");
-            fprintf(lv, "%d:%lld:%lld:%lld",a->station->identifiant,a->station->capacite,a->station->consommation,a->station->analyse);
+            printf("station:capacité:consommation totale des consommateurs:analyse de la comsommation");
+            fprintf(fichier, "%d:%lld:%lld:%lld",a->station->identifiant,a->station->capacite,a->station->consommation,a->station->analyse);
             if(a->fg!=NULL){
-                creationfichieranalyse(a->fg, type,pareil);
+                creationfichieranalyse(a->fg, type,consommateur);
             }
-            if(a->fd!=NULL){
-                creationfichieranalyse(a->fd, type,pareil);
+            else if(a->fd!=NULL){
+                creationfichieranalyse(a->fd, type,consommateur);
+            }
+            else{
+                fclose(fichier);
+                return;
             }
         }
     }
 }
 
+//procédure qui va libérer l'espace alloué par l'arbre en commençant par les feuilles
 void liberationArbre(Arbre* a){
     if(a==NULL){
         return;
@@ -281,14 +315,12 @@ void liberationArbre(Arbre* a){
 int main(int arc, char **argv) {
     Arbre* a= recuperationfichier("fichier",argv[1],argv[2]);
     recuperationconsommation(a,argv[1]);
-    creationfichieranalyse(a,argv[1],luciejetelaissemodifier);
+    creationfichieranalyse(a,argv[1],argv[2]);
     liberationArbre(a);
     return 0;
 }
 
 /*
-
  message.txt
 9 Ko
-
 */
