4971862ef2d5aa62afcad89bf625092bfa7864e4
lulu597 <natation0912@gmail.com>
2024-12-19 13:12:27 +0100

    Update fonction.c

--- src/fonction.c
+++ src/fonction.c
@@ -12,14 +12,16 @@ Arbre *creationArbre(int identifiant , long long capacite){
     if(d==NULL){
         exit(3);
     }
-    d->station->identifiant=identifiant;
-    d->station->capacite=capacite;
+
+    d->station.identifiant=identifiant;
+    d->station.capacite=capacite;
     d->fg=NULL;
     d->fd=NULL;
     d->equilibre=0;
-    d->station->consommation=0;
-    d->station->analyse=0;
-    d->station->valeur_absolue=0;
+    d->station.consommation=0;
+    d->station.analyse=0;
+    //d->station.valeur_absolue=0;
+
     return d;
 }
 
@@ -53,7 +55,7 @@ Arbre * rotationdroite(Arbre *a){
     pivot->fd=a;
     a->equilibre=eq_a-min(eq_p,0)+1;
     pivot->equilibre=max(max(eq_a+2,eq_a+eq_p+2),eq_p+1);
-    a=pivot;
+    a=pivot;  
     return a;
 }
 
@@ -78,7 +80,7 @@ Arbre* equilibrage(Arbre *a){
             return doublerotationgauche(a);
         }
     }
-    if(a->equilibre<=2){
+    if(a->equilibre<=-2){
         if(a->fd<=0){
             return rotationdroite(a);
         }
@@ -94,11 +96,14 @@ Arbre *insertionArbre(Arbre *a, int id, long long capacite, int* h){
     if (a == NULL) {
         *h = 1;
         return creationArbre(id,capacite);
-    } else if (id < a->station->identifiant) {
+        
+    } else if (id < a->station.identifiant) {
         a->fg = insertionArbre(a->fg,id,capacite,h);
         *h = -*h;
-    } else if (id > a->station->identifiant) {
+        
+    } else if (id > a->station.identifiant) {
         a->fd = insertionArbre(a->fd,id,capacite,h);
+        
     } else {
         *h = 0;
         return a;
@@ -116,134 +121,199 @@ Arbre *insertionArbre(Arbre *a, int id, long long capacite, int* h){
     return a;
 }
 
+		
+// Fonction qui permet de récupérer les stations dans le fichier pour ensuite les ajouter dans un arbre AVL
+Arbre* recuperationfichier(char * nomfichier, char *type){
+    FILE *fichier=NULL;
+    Arbre* n = NULL;
+    int ident;
+    char tab[100]; 
+    int h=0;
+    long long capa;
+
+    fichier = fopen(nomfichier,"r");
+    if(fichier ==NULL){
+        exit(4);
+    }
+    
+fgets(tab, 99, fichier);
+if(strcmp(type, "hvb")==0){
+	while(fscanf(fichier,"%*[^;];%d;%*[^;];%*[^;];%*[^;];%*[^;];%lld;%*s",&ident,&capa)==2){
+		if(ident<0){
+			exit(5);
+			}
+		if(capa<0){
+			exit(5);
+			}
+		n = insertionArbre(n, ident, capa, &h);
+    	}
+}
+if(strcmp(type, "hva")==0){
+	while(fscanf(fichier,"%*[^;];%*[^;];%d;%*[^;];%*[^;];%*[^;];%lld;%*s",&ident,&capa)==2){
+		if(ident<0){
+			exit(5);
+			}
+		if(capa<0){
+			exit(5);
+			}
+		n = insertionArbre(n, ident, capa, &h);
+    	}
+}
+if(strcmp(type, "lv")==0){
+	while(fscanf(fichier,"%*[^;];%*[^;];%*[^;];%d;%*[^;];%*[^;];%lld;%*s",&ident,&capa)==2){
+		if(ident<0){
+			exit(5);
+			}
+		if(capa<0){
+			exit(5);
+			}
+		n = insertionArbre(n, ident, capa, &h);
+    	}
+}
+	
+    fclose(fichier);
+    return n;
+}
+
 //procédure de recherche à partir des identifiants de la station pour ajouter la consommation du consommateur à celle totale
 void ajoutconsommation(Arbre *a, long long k, int id){
     if(a==NULL){
         exit(2);
     }
-    if(id<a->station->identifiant){
+    if(id<a->station.identifiant){
         ajoutconsommation(a->fg,k,id);
     }
-    if(id>a->station->identifiant){
+    if(id>a->station.identifiant){
         ajoutconsommation(a->fd,k,id);
     }
-    if(id==a->station->identifiant){
-        a->station->consommation+=k;
+    if(id==a->station.identifiant){
+        a->station.consommation+=k;
     }
 }
 
 //procédure qui lit les données filtrées depuis un fichier CSV en fonction d'un type donné et met à jour la consommation totale en y ajoutant les consommations associées aux consommateurs
 void recuperationconsommation(Arbre*a, char *type){
     if(a==NULL){
-        exit(2);
+        exit(9);
     }
+   
+   char tab[100]; 
+   int c;
     FILE *fichier=fopen("donnees_filtrees.csv","r");
     if(fichier ==NULL){
         exit(4);
     }
-
-    int id_temporaire = 0;
+    int id_station=0;
     long long k = 0;
+    fgets(tab, 99, fichier);
     if(strcmp(type,"hvb")==0){
-        while (fscanf(fichier,"%*[^;];%d;%*[^;];%*[^;];%*[^;];%*[^;];%*[^;];%lld",&id_temporaire,&k) == 2){
-            ajoutconsommation(a,k,id_temporaire);
-        }
+	while(fscanf(fichier,"%*[^;];%d;%*[^;];%*[^;];%*[^;];%*[^;];%*[^;];%lld",&id_station,&k)==2){
+		if(k<0){
+			exit(5);
+			}
+		if(id_station<0){
+			exit(5);
+			}
+		ajoutconsommation(a,k,id_station);
+    	}
     }
+    
     if(strcmp(type,"hva")==0){
-        while (fscanf(fichier,"%*[^;];%*[^;];%d;%*[^;];%*[^;];%*[^;];%*[^;];%lld",&id_temporaire,&k) == 2){
-            ajoutconsommation(a,k,id_temporaire);
-        }
+        while(fscanf(fichier,"%*[^;];%*[^;];%d;%*[^;];%*[^;];%*[^;];%*[^;];%lld",&id_station,&k)==2){
+		if(k<0){
+			exit(5);
+			}
+		if(id_station<0){
+			exit(5);
+			}
+		ajoutconsommation(a,k,id_station);
+    	}
     }
+    
     if(strcmp(type,"lv")==0){
-        while (fscanf(fichier,"%*[^;];%*[^;];%*[^;];%d;%*[^;];%*[^;];%*[^;];%lld",&id_temporaire, &k) == 2){
-            ajoutconsommation(a,k,id_temporaire);
-        }
-    }
-}
-
-// Fonction qui permet de récupérer les stations dans le fichier pour ensuite les ajouter dans un arbre AVL
-Arbre* recuperationfichier(char * nomfichier, char *type, char *consommateur){
-    FILE *fichier=fopen(nomfichier,"r");
-    if(fichier ==NULL){
-        exit(4);
-    }
-    Arbre* n=NULL;
-    int ident;
-    int h=0;
-    long long capa;
-    while(fscanf(fichier,"%d;%*[^;];%*[^;];%*[^;];%*[^;];%*[^;];%lld",&ident,&capa)==2){
-        n = insertionArbre(n, ident, capa, &h);
+        while(fscanf(fichier,"%*[^;];%*[^;];%*[^;];%d;%*[^;];%*[^;];%*[^;];%lld",&id_station,&k)==2){
+		if(k<0){
+			exit(5);
+			}
+		if(id_station<0){
+			exit(5);
+			}
+		ajoutconsommation(a,k,id_station);
+    	}
     }
     fclose(fichier);
-    return n;
 }
 
 //Une procédure qui calcule la différence entre la capacité d'une station et la consommation des consommateurs avec un parcours infixe
 void sommeconsommation(Arbre* a){
-
     if(a==NULL){
-        exit(2);
+        return;
     }
+    
     sommeconsommation(a->fg);
-    a->station->analyse = (a->station->capacite - a->station->consommation);
-    a->station->valeur_absolue=a->station->analyse;
-    if(a->station->valeur_absolue<0){
-        a->station->valeur_absolue=-a->station->valeur_absolue;
-    }
+    a->station.analyse = (a->station.capacite - a->station.consommation);
+    //a->station.valeur_absolue=a->station.analyse;
+    //if(a->station.valeur_absolue<0){
+       // a->station.valeur_absolue=-a->station.valeur_absolue;
+   // }
     sommeconsommation(a->fd);
 }
 
 //procédure qui va écrire dans un fichier les données en parcourant l'arbre
 void ecriturefichier(Arbre * a, FILE *nomfichier){
-    fprintf(nomfichier,"station:capacité:consommation totale des consommateurs:analyse de la comsommation");
-    fprintf(nomfichier,"%d:%lld:%lld:%lld",a->station->identifiant,a->station->capacite,a->station->consommation,a->station->analyse);
     if(a->fg!=NULL){
         ecriturefichier(a->fg,nomfichier);
     }
-    else if(a->fd!=NULL){
+    fprintf(nomfichier,"%d:%lld:%lld:%lld\n",a->station.identifiant,a->station.capacite,a->station.consommation,a->station.analyse);
+    if(a->fd!=NULL){
         ecriturefichier(a->fd,nomfichier);
     }
 }
-
 
 //procédure qui va créer les fichiers avec les données de surproduction ou non de chaque station en fonction de ce que l'utilisateur aura demandé
 void creationfichieranalyse(Arbre *a, char *type, char * consommateur){
     if(a==NULL){
         exit(2);
     }
+ 
     if(strcmp(type,"hvb")==0){
         FILE *fichier = fopen("hvb_comp.csv", "w");
         if (fichier == NULL) {
             exit(4);
         }
+        fprintf(fichier,"station:capacité:consommation totale des consommateurs:analyse de la comsommation\n");
         ecriturefichier(a,fichier);
         fclose(fichier);
     }
+    
     if(strcmp(type,"hva")==0){
         FILE *fichier = fopen("hva_comp.csv", "w");
         if (fichier == NULL) {
             exit(4);
         }
+        fprintf(fichier,"station:capacité:consommation totale des consommateurs:analyse de la comsommation\n");
         ecriturefichier(a,fichier);
-        fclose(fichier);
-    }
+            fclose(fichier);
+        }
+
     if(strcmp(type,"lv")==0){
         if(strcmp(consommateur,"comp")==0){
             FILE *fichier = fopen("lv_comp.csv", "w");
             if (fichier == NULL) {
                 exit(4);
             }
-            ecriturefichier(a, fichier);
+            fprintf(fichier,"station:capacité:consommation totale des consommateurs:analyse de la comsommation\n");
+            ecriturefichier(a,fichier);
             fclose(fichier);
-            }
         }
         if(strcmp(consommateur,"indiv")==0) {
             FILE *fichier = fopen("lv_indiv.csv", "w");
             if (fichier == NULL) {
                 exit(4);
             }
-            ecriturefichier(a, fichier);
-            fclose(fichier);
+            fprintf(fichier,"station:capacité:consommation totale des consommateurs:analyse de la comsommation\n");
+            ecriturefichier(a,fichier);
+                fclose(fichier);
         }
         if(strcmp(consommateur,"all")==0) {
             FILE *fichier = fopen("lv_all.csv", "w");
@@ -251,25 +321,25 @@ void creationfichieranalyse(Arbre *a, char *type, char * consommateur){
             if (fichier == NULL) {
                 exit(4);
             }
-            /*if (valeur_absolue == NULL){
-                exit(4);
-            }*/
-            //fprintf(valeur_absolue,"station:capacité:consommation totale des consommateurs:analyse de la comsommation en valeur absolue\n");
-            //fprintf(valeur_absolue, "%d:%lld:%lld:%lld\n",a->station->identifiant,a->station->capacite,a->station->consommation,a->station->valeur_absolue);
-            ecriturefichier(a, fichier);
-            fclose(fichier);
-            //fclose(valeur_absolue)
+            //if (valeur_absolue == NULL) {
+             //   exit(4);
+            //}
+            fprintf(fichier,"station:capacité:consommation totale des consommateurs:analyse de la comsommation\n");
+            //fprintf(valeu_absolue,"station:capacité:consommation totale des consommateurs:analyse de la comsommation en valeur absolue\n");
+            //fprintf(valeur_absolue, "%d:%lld:%lld:%lld\n",a->station.identifiant,a->station.capacite,a->station.consommation,a->station.valeur_absolue);
+            ecriturefichier(a,fichier);
+                fclose(fichier);
+                //fclose(valeur_absolue);
             }
         }
+}
 
 //procédure qui va libérer l'espace alloué par l'arbre en commençant par les feuilles
 void liberationArbre(Arbre* a){
     if(a==NULL){
-        return;
+        exit(2);
     }
     liberationArbre(a->fg);
     liberationArbre(a->fd);
     free(a);
 }
-
-
