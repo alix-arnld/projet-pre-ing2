da9237843bb96463ff8735dcc92a80af3fd91413
lulu597 <natation0912@gmail.com>
2024-12-19 16:54:29 +0100

    Update fonction.c

--- src/fonction.c
+++ src/fonction.c
@@ -277,7 +277,7 @@ void creationfichieranalyse(Arbre *a, char *type, char * consommateur){
     }
  
     if(strcmp(type,"hvb")==0){
-        FILE *fichier = fopen("hvb_comp.csv", "w");
+        FILE *fichier = fopen("../tmp/hvb_comp.csv", "w");
         if (fichier == NULL) {
             exit(4);
         }
@@ -287,7 +287,7 @@ void creationfichieranalyse(Arbre *a, char *type, char * consommateur){
     }
     
     if(strcmp(type,"hva")==0){
-        FILE *fichier = fopen("hva_comp.csv", "w");
+        FILE *fichier = fopen("../tmp/hva_comp.csv", "w");
         if (fichier == NULL) {
             exit(4);
         }
@@ -298,7 +298,7 @@ void creationfichieranalyse(Arbre *a, char *type, char * consommateur){
 
     if(strcmp(type,"lv")==0){
         if(strcmp(consommateur,"comp")==0){
-            FILE *fichier = fopen("lv_comp.csv", "w");
+            FILE *fichier = fopen("../tmp/lv_comp.csv", "w");
             if (fichier == NULL) {
                 exit(4);
             }
@@ -307,7 +307,7 @@ void creationfichieranalyse(Arbre *a, char *type, char * consommateur){
             fclose(fichier);
         }
         if(strcmp(consommateur,"indiv")==0) {
-            FILE *fichier = fopen("lv_indiv.csv", "w");
+            FILE *fichier = fopen("../tmp/lv_indiv.csv", "w");
             if (fichier == NULL) {
                 exit(4);
             }
@@ -316,7 +316,7 @@ void creationfichieranalyse(Arbre *a, char *type, char * consommateur){
                 fclose(fichier);
         }
         if(strcmp(consommateur,"all")==0) {
-            FILE *fichier = fopen("lv_all.csv", "w");
+            FILE *fichier = fopen("../tmp/lv_all.csv", "w");
             //FILE *valeur_absolue = fopen("lv_valeur_absolue.csv", "w");
             if (fichier == NULL) {
                 exit(4);
