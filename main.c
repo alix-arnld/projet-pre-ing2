515b6093d1b1f22176fc233e927ef8c6ba36f775
lulu597 <natation0912@gmail.com>
2024-12-20 19:25:18 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "fonction.h"
 
 int main(int argc,char** argv) {
-    Arbre* a= recuperationfichier("donnees_capacites.csv", argv[1]);
+    Arbre* a= recuperationfichier("../tmp/donnees_capacites.csv", argv[1]);
     recuperationconsommation(a,argv[1]);
     sommeconsommation(a);
     creationfichieranalyse(a,argv[1],argv[2]);
